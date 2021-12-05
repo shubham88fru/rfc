bf552f3fa9beeff63aa92922101e07e74b48c472
Shubham Singh <shubham88fru@yahoo.com>
2021-12-05 17:34:13 +0530

    component/jsx rules

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react'; //Not needed since react 17+
 import ReactDOM from 'react-dom';
 
-// FC can be function declaration
+/**
+ * Component Rules
+ */
+// 1) FC can be function declaration
 // or function expressions.
-// Either way, function name should
+// 2) Either way, function name should
 // be capitalize.
+// 3) React Component must return JSX.
+
+/**
+ * JSX Rules
+ */
+// 1) Coponent should return just single
+// element
+// 2) props should use camle case instead of
+// kebab-case
 const Greeting = () => {
   return <h1>Hello World!</h1>;
 };
