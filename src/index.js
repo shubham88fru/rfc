9d9268c44c5a88bf8aebc03dae01fad29f9e15a0
Shubham Singh <shubham88fru@yahoo.com>
2021-12-14 11:23:28 +0530

    add id

--- src/index.js
+++ src/index.js
@@ -21,11 +21,13 @@ import './index.css';
 // kebab-case
 const books = [
   {
+    id: 1,
     src: 'https://images-eu.ssl-images-amazon.com/images/I/517h-u1AQlL._SX198_BO1,204,203,200_QL40_FMwebp_.jpg',
     title: 'I Love You To the Moon and Back',
     author: 'Amelia Hepworth',
   },
   {
+    id: 2,
     src: 'https://images-eu.ssl-images-amazon.com/images/I/517h-u1AQlL._SX198_BO1,204,203,200_QL40_FMwebp_.jpg',
     title: 'I Love You To the Moon and Back v2',
     author: 'Amelia Hepworth',
@@ -33,8 +35,8 @@ const books = [
 ];
 
 const BookList = () => {
-  const booklist = books.map(({ src, title, author }) => (
-    <Book key={title} src={src} title={title} author={author} />
+  const booklist = books.map(({ id, src, title, author }) => (
+    <Book key={id} src={src} title={title} author={author} />
   ));
   return <section className="booklist">{booklist}</section>;
 };
